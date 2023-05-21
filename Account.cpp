ab5cdd987cef0dc3bf2a64fa8d13368ee77d09fa
groc-coding-school <groc.coders.schools@gmail.com>
2023-05-21 03:20:02 +0300

    New changes

---
+++ Account.cpp
@@ -0,0 +1,16 @@
+//class to represent bank account
+class Account
+{
+    int one;
+    public:
+    Account();
+    ~Account();
+};
+
+//main function to run the program
+int main()
+{
+    //creating object called banks
+    Account banks;
+    return 0;
+}
